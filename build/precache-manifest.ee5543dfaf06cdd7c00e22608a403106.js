8a3ae63037c4349ba86dc264b6e163c3da31f4a3
Umer-Naseer-Jovindah <99118963+Umer-Naseer-Jovindah@users.noreply.github.com>
2022-07-28 23:02:51 +0200

    make it better

---
+++ build/precache-manifest.ee5543dfaf06cdd7c00e22608a403106.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94df222a48be582f34c270ee60072956",
+    "url": "/My-Notes/index.html"
+  },
+  {
+    "revision": "eaa1195ee5dceb2a2e56",
+    "url": "/My-Notes/static/js/2.6e44967c.chunk.js"
+  },
+  {
+    "revision": "1e7dbeb9e1672ffde319",
+    "url": "/My-Notes/static/js/main.855cf565.chunk.js"
+  },
+  {
+    "revision": "5d3d1b5a4fd306a0c289",
+    "url": "/My-Notes/static/js/runtime-main.1962decf.js"
+  }
+]);
